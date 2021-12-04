7b8976e84367794dae886b264f17686aaa85cd69
ricardofer2412 <richardcasanovac@gmail.com>
2021-12-04 17:49:48 -0500

    add highlight effect menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -43,7 +43,6 @@ const Logo = styled.img`
         display: block;
         width: 100%
     }`;
-
 const NavMenu = styled.div`
   align-items: center;
   display: flex;
@@ -54,33 +53,52 @@ const NavMenu = styled.div`
   padding: 0px;
   position: relative;
   margin-right: auto;
-  margin:left: 25px;
-
+  margin-left: 25px;
   a {
     display: flex;
     align-items: center;
-    padding: 0 10px;
-
+    padding: 0 12px;
     img {
       height: 20px;
       min-width: 20px;
       width: 20px;
       z-index: auto;
     }
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
   }
-
-  span {
-    color: rgb(249, 249, 249);
-    font-size: 13px;
-    letter-spacing: 1.42px;
-    line-height: 1.08;
-    padding: 2px 0px;
-    white-space: nowrap;
-    postion: relative;
-  }
-
-  @media (max-width: 768px) {
+  /* @media (max-width: 768px) {
     display: none;
-  }
+  } */
 `;
 export default Header;
