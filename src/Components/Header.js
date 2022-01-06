753d152002adaa72b69c6e3ff75dbe89ce6d208b
ricardofer2412 <richardcasanovac@gmail.com>
2022-01-05 19:00:23 -0500

    add firebase

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,6 +30,7 @@ const Header = (props) => {
           <span>SERIES</span>
         </a>
       </NavMenu>
+      <Login>LOGIN</Login>
     </Nav>
   );
 };
@@ -63,6 +64,23 @@ const Logo = styled.img`
         display: block;
         width: 100%
     }`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
 const NavMenu = styled.div`
   align-items: center;
   display: flex;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_mg7rPCPRcytP0wTrE9tSBxSe5N4PJ5g",
+  authDomain: "disney-plus-clone-7631b.firebaseapp.com",
+  projectId: "disney-plus-clone-7631b",
+  storageBucket: "disney-plus-clone-7631b.appspot.com",
+  messagingSenderId: "808767791012",
+  appId: "1:808767791012:web:68cecda2d4214905c5ab31",
+  measurementId: "G-J3YWL8BL7M",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
